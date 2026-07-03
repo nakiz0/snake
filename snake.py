a487da2768ad5fb680e85807dd859892723e983d
Amrit <amritxtamu132@gmail.com>
2026-07-03 22:28:59 +0530

    add

---
+++ snake.py
@@ -0,0 +1,13 @@
+import pygame as pg
+
+pg.init()
+
+screen=pg.display.set_mode((640,640))
+
+running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+pg.quit()
